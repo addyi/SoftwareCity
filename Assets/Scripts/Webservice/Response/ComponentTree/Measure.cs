5d855d5b62c4ee4db0e845d640d18e60f7b52c32
Adrian Renner <a.renner+git@eosn.de>
2017-07-01 09:07:28 +0200

    Add WebService from command line projekt

---
+++ Assets/Scripts/Webservice/Response/ComponentTree/Measure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Webservice.Response.ComponentTree
+{
+    [Serializable]
+    public class Measure
+    {
+        public string metric;
+        public string value;
+
+        public override string ToString()
+        {
+            return string.Format("(Measure: Metric={0}, Value={1})", metric, value);
+        }
+    }
+}
---
+++ Assets/Scripts/Webservice/UriBuilding/SqUriBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Webservice.UriBuilding
+{
+    public abstract class SqUriBuilder : ISqUriBuilder
+    {
+        protected UriBuilder uriBuilder;
+
+        protected SqUriBuilder(string baseUri)
+        {
+            uriBuilder = new UriBuilder(baseUri);
+        }
+
+        public ISqUriBuilder AppendToPath(string pathToAppend)
+        {
+            if (uriBuilder.Path != null && uriBuilder.Query.Length > 1)
+                uriBuilder.Path = uriBuilder.Path.Substring(1) + "/" + pathToAppend;
+            else
+                uriBuilder.Path = pathToAppend;
+            return this;
+        }
+
+        public ISqUriBuilder AppendToQuery(string queryToAppend)
+        {
+            if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
+                uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + queryToAppend;
+            else
+                uriBuilder.Query = queryToAppend;
+            return this;
+        }
+
+        public ISqUriBuilder UserCredentials(string username, string password)
+        {
+            uriBuilder.Password = password;
+            uriBuilder.UserName = username;
+            return this;
+        }
+        public Uri GetSqUri()
+        {
+            return uriBuilder.Uri;
+        }
+
+        public abstract ISqUriBuilder MetricKeys(string metricKeys);
+        public abstract ISqUriBuilder Page(int page);
+        public abstract ISqUriBuilder PageSize(int pageSize);
+        public abstract ISqUriBuilder ProjectKey(string projektKey);
+
+    }
+}
