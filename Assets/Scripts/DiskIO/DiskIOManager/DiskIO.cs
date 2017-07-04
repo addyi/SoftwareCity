667cd509ed596714b9d3a83d264a44f3ea018ba6
ysghairi <ysghairi@evoila.de>
2017-07-04 08:38:17 +0200

    add Disk-io component

---
+++ Assets/Scripts/DiskIO/DiskIOManager/DiskIO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using QAware.DataStorage.DataEntertainment;
+using QAware.DataStorage.ReadConfigFile;
+
+namespace QAware.DataStorage.DiskIOManager
+{
+    [Serializable]
+    public class DiskIO
+    {
+        private ConfigFile configFile;
+        private ComponentTree componentTree;
+        private SQCredential credentials;
+        private readonly string directoryPath = @"/Users/yacinesghairi/Desktop";
+
+        public DiskIO(){
+        }
+
+        public ConfigFile ConfigFile { get => configFile; set => configFile = value; }
+        public ComponentTree ComponentTree { get => componentTree; set => componentTree = value; }
+        public SQCredential Credentials { get => credentials; set => credentials = value; }
+
+        public List<Metric> GetAvailableMetrics(ConfigFile configFile){
+            return configFile.ReadKonfigFile("/Users/yacinesghairi/Desktop/MyStacks/QAware-statics/av-metrics/available.json");
+        }
+
+        public ComponentTree GetSavedTree(string fileName)
+        {	
+			string file = Path.Combine(directoryPath, fileName);
+            DiskIOStream<ComponentTree> dskioStream = new DiskIOStream<ComponentTree>();
+            return dskioStream.DeserializeDiskIOItem(file);
+
+        }
+
+		public void SetSavedTree(string fileName, ComponentTree componentTree)
+		{
+			string file = Path.Combine(directoryPath, fileName);
+            DiskIOStream<ComponentTree> dskioStream = new DiskIOStream<ComponentTree>();
+            dskioStream.SerializeDiskIOItem(file, componentTree);
+		}
+    }
+}
---
+++ Assets/Scripts/DiskIO/DiskIOManager/DiskIOStream.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace QAware.DataStorage.DiskIOManager
+{
+    public class DiskIOStream<Type>
+    {
+        public DiskIOStream(){
+        }
+
+        public void SerializeDiskIOItem(string file, Type item)
+        {
+            //serialize
+            using (Stream stream = File.Open(file, FileMode.Create))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                bformatter.Serialize(stream, item);
+            }
+        }
+
+        public Type DeserializeDiskIOItem(string file)
+        {
+            //deserialize
+            using (Stream stream = File.Open(file, FileMode.Open))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (Type)bformatter.Deserialize(stream);
+
+            }
+           
+        }
+    }
+}
