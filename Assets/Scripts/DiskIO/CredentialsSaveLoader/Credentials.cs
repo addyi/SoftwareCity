96e54d7c4d648c05a9b8a00f05937b03c66772c2
YacineSghairi <yacine.sghairi@gmx.net>
2017-07-06 20:33:34 +0200

    class for manage (save/load) user credentials

---
+++ Assets/Scripts/DiskIO/CredentialsSaveLoader/Credentials.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace DiskIO.CredentialsSaveLoader
+{
+    public class Credentials
+    {
+        public Credentials(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public void SaveCredentials()
+        {
+            PlayerPrefs.SetString("Username", this.Username);
+            PlayerPrefs.SetString("Password", this.Password);
+        }
+
+        public string LoadUsername(){
+            return PlayerPrefs.GetString("Username");
+        }
+
+		public string LoadPassword()
+		{
+			return PlayerPrefs.GetString("Password");
+		}
+    }
+}
