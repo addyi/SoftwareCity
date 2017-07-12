99e9189726ec30f7758af1e96553abf647de06d3
richard <richard.mezler@th-bingen.de>
2017-07-12 11:30:32 +0200

    feat(config-fenster): buttons are now dynamically added and have listeners

--- Assets/Scripts/KonfigFenster/ButtonEventHandling/WriteOnPanel/ButtonPool.cs
+++ Assets/Scripts/KonfigFenster/ButtonEventHandling/WriteOnPanel/ButtonPool.cs
@@ -10,14 +10,29 @@ namespace ConfigurationWindow.ButtonEventHandling.WriteOnPanel
 
         public GameObject buttonPrefab;
         public GameObject panelScrollView;
+
+        public GameObject actualPanel;
+        public GameObject nextPanel;
+
+        private List<string> allProjects;
         // Use this for initialization
-        void Start()
+        void Awake()
         {
-            GameObject button = Instantiate(buttonPrefab) as GameObject;
-            button.transform.SetParent(panelScrollView.transform);
-            Debug.Log(panelScrollView.transform.position.z);
+            allProjects = new List<string> { "LocalProject", "Online Project", "Java Project", "Foo Project" };
+            foreach (string s in allProjects)
+            {
+                GameObject button = Instantiate(buttonPrefab) as GameObject;
+                button.transform.SetParent(panelScrollView.transform);
+                button.transform.localPosition = new Vector3(button.transform.localPosition.x, button.transform.localPosition.y, 0.0f);
+                button.GetComponentInChildren<Text>().text = s;
+                button.GetComponent<Button>().onClick.AddListener(Clicked);
+            }
         }
 
-
+        void Clicked()
+        {
+            actualPanel.SetActive(false);
+            nextPanel.SetActive(true);
+        }
     }
 }
