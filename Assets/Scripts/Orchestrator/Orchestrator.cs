8081f89f8d42eb8efc20716fe0d1ab193a007b81
Adrian Renner <a.renner+git@eosn.de>
2017-07-13 15:14:35 +0200

    Implement LoadProject(s)

--- Assets/Scripts/Orchestrator/Orchestrator.cs
+++ Assets/Scripts/Orchestrator/Orchestrator.cs
@@ -17,79 +17,13 @@ namespace Orchestrator
         private string baseUri = "";
         private string username = "";
         private string password = "";
+        private string selectedProjectKey = "";
 
 
         // Use this for initialization
         void Start()
         {
-            //const string metricKeys = "ncloc,bugs,vulnerabilities,code_smells,violations,functions,coverage,test_success_density,comment_lines_density";
-            //const string projectKey = "geo-quiz-app";
-
-            //// TODO ADDYI REMOVE DEBUG LOGS
-            //StartCoroutine(WebInterface.WebRequest<Auth>(
-            //    new SqAuthValidationUriBuilder(baseUri, username, password).GetSqUri(),
-            //    (res, err) =>
-            //    {
-            //        switch (err)
-            //        {
-            //            case 200:
-            //                Debug.Log("Addyi Auth:" + res.valid.ToString());
-            //                break;
-            //            default:
-            //                Debug.Log("Addyi ResponseCode: " + err);
-            //                break;
-
-            //        }
-            //    }));
-
-            //StartCoroutine(WebInterface.WebRequest<List<SQProject>>(
-            //   new SqProjectUriBuilder(baseUri).UserCredentials(username, password).GetSqUri(),
-            //   (res, err) =>
-            //   {
-            //       switch (err)
-            //       {
-            //           case 200:
-            //               res.ForEach((projekt) =>
-            //               {
-            //                   Debug.Log("Addyi Projekt: " + projekt.ToString());
-            //               });
-            //               break;
-            //           default:
-            //               Debug.Log("Addyi ResponseCode: " + err);
-            //               break;
-
-            //       }
-            //   }));
-
-            //StartCoroutine(WebInterface.WebRequest<ComponentTree>(
-            //   new SqComponentTreeUriBuilder(baseUri, projectKey, metricKeys)
-            //        .UserCredentials(username, password).GetSqUri(),
-            //   (System.Action<ComponentTree, long>)((res, err) =>
-            //   {
-            //       switch (err)
-            //       {
-            //           case 200:
-            //               Debug.Log(res.baseComponent.ToString());
-            //               Debug.Log(res.paging.ToString());
-            //               IProjectTree ProjectTree = new Model();
-
-            //               // List<Webservice.Response.ComponentTree.Component> components = res.components;
-            //               //components.Sort();
-
-
-            //               ProjectTree.BuildProjectTree(res.baseComponent, res.components);
-            //               Debug.Log(ProjectTree.GetTree().ToString());
-
-            //               break;
-            //           default:
-            //               Debug.Log("Addyi ResponseCode: " + err);
-            //               break;
-
-            //       }
-
-            //   })));
-
-
+            
         }
 
         public void GetLocalProject()
@@ -124,7 +58,7 @@ namespace Orchestrator
                 }));
         }
 
-        public void SelectedMetrics(string[] KeysSelectedMetrics)
+        public void SelectedMetrics(Metric[] KeysSelectedMetrics)
         {
             throw new NotImplementedException();
         }
@@ -137,6 +71,68 @@ namespace Orchestrator
         public void ShowCity()
         {
             throw new NotImplementedException();
+        }
+
+        public void SelectedProject(string projectKey)
+        {
+            selectedProjectKey = projectKey;
+        }
+
+        private void LoadProjects()
+        {
+            SqProjectUriBuilder uriBuilder = new SqProjectUriBuilder(baseUri);
+            if (username != "" && password != "")
+                uriBuilder.UserCredentials(username, password);
+
+            StartCoroutine(WebInterface.WebRequest<List<SQProject>>(
+               uriBuilder.GetSqUri(),
+               (res, err) =>
+               {
+                   switch (err)
+                   {
+                       case 200:
+                           res.ForEach((projekt) =>
+                           {
+                               Debug.Log("Addyi Projekt: " + projekt.ToString());
+                           });
+                           break;
+                       default:
+                           Debug.Log("Addyi ResponseCode: " + err);
+                           break;
+
+                   }
+               }));
+        }
+
+        private void LoadProject()
+        {
+            //StartCoroutine(WebInterface.WebRequest<ComponentTree>(
+            //   new SqComponentTreeUriBuilder(baseUri, projectKey, metricKeys)
+            //        .UserCredentials(username, password).GetSqUri(),
+            //   (System.Action<ComponentTree, long>)((res, err) =>
+            //   {
+            //       switch (err)
+            //       {
+            //           case 200:
+            //               Debug.Log(res.baseComponent.ToString());
+            //               Debug.Log(res.paging.ToString());
+            //               IProjectTree ProjectTree = new Model();
+
+            //               // List<Webservice.Response.ComponentTree.Component> components = res.components;
+            //               //components.Sort();
+
+
+            //               ProjectTree.BuildProjectTree(res.baseComponent, res.components);
+            //               Debug.Log(ProjectTree.GetTree().ToString());
+
+            //               break;
+            //           default:
+            //               Debug.Log("Addyi ResponseCode: " + err);
+            //               break;
+
+            //       }
+
+            //   })));
         }
     }
 }
