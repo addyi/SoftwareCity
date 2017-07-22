38958b0bd4e0d46ceb43d343d63352ca7a5b1d7f
Adrian Renner <a.renner+git@eosn.de>
2017-07-22 16:40:18 +0200

    Add MaxForMetric method

--- Assets/Scripts/DataModel/ProjectTree/Components/TreeComponent.cs
+++ Assets/Scripts/DataModel/ProjectTree/Components/TreeComponent.cs
@@ -34,6 +34,13 @@ namespace DataModel.ProjectTree.Components
 
         public abstract TreeComponent InsertComponentAt(string[] path, TreeComponent component);
 
+        /// <summary>
+        /// Max value of the defined metic in howl tree
+        /// </summary>
+        /// <param name="MetricKey">sonarqube metric key</param>
+        /// <returns>max value</returns>
+        public abstract double GetMaxForMetric(string MetricKey);
+
         public virtual TreeComponent UpdateComponent(TreeComponent component)
         {
             if (component != null && Name == component.Name)
