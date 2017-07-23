8856b4eb9e2e18c911940781502a7b32d5110bad
Tobias Weiß <tobias.weiss.web@web.de>
2017-07-23 15:19:16 +0200

    test(rendering-test): add ComponentProducerTest

---
+++ Assets/Editor/SoftwareCity/ComponentProducerTest.cs
@@ -0,0 +1,41 @@
+﻿using DataModel.ProjectTree.Components;
+using NUnit.Framework;
+using SoftwareCity.Rendering.Utils;
+using System.Collections.Generic;
+using Webservice.Response.ComponentTree;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ComponentProducerTest {
+
+	[Test]
+	public void GenerateDocumentTest() {
+        GameObject componentProducerTestGameObject = new GameObject();
+        componentProducerTestGameObject.AddComponent<ComponentProducer>();
+
+        GameObject testGameObject = componentProducerTestGameObject.GetComponent<ComponentProducer>().GenerateDocument(GetFilTreeComponent());
+
+        Assert.AreEqual(testGameObject.GetComponent<Renderer>().shadowCastingMode, ShadowCastingMode.Off);
+        Assert.AreEqual(testGameObject.GetComponent<Renderer>().lightProbeUsage, LightProbeUsage.Off);
+        Assert.AreEqual(testGameObject.GetComponent<Renderer>().reflectionProbeUsage, ReflectionProbeUsage.Off);
+        Assert.False(testGameObject.GetComponent<Renderer>().receiveShadows);
+        Assert.False(testGameObject.GetComponent<Collider>().enabled);
+        Assert.False(testGameObject.GetComponent<Renderer>().enabled);
+        Assert.AreEqual(testGameObject.transform.position, Vector3.zero);
+    }
+
+    private FilComponent GetFilTreeComponent()
+    {
+        SqComponent sqComponent = new SqComponent();
+        sqComponent.id = "id";
+        sqComponent.key = "key";
+        sqComponent.name = "name";
+        sqComponent.qualifier = "FIL";
+        sqComponent.path = "path";
+        sqComponent.language = "language";
+
+        sqComponent.measures = new List<Measure>();
+
+        return new FilComponent(sqComponent);
+    }
+}
