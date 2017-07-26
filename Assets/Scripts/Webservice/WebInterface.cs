57033c604070cf5748265cda4825fc37f757b857
Adrian Renner <a.renner+git@eosn.de>
2017-07-26 09:05:24 +0200

    Comment: WebInterface

--- Assets/Scripts/Webservice/WebInterface.cs
+++ Assets/Scripts/Webservice/WebInterface.cs
@@ -2,13 +2,22 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
-using Webservice.Response.ArrayResponseSQProject;
-using Webservice.Response.Project;
 
 namespace Webservice
 {
+    /// <summary>
+    /// This is the class responsible for requesting data from the SonarQube API
+    /// </summary>
     public static class WebInterface
     {
+        /// <summary>
+        /// Request JSON Data from the SonarQube API for a specific Uri and deserializing it to C# objects
+        /// </summary>
+        /// <typeparam name="T">Deserializing Type</typeparam>
+        /// <param name="uri">Uri for the request</param>
+        /// <param name="callback">Async response methode. It has the parameters T and long. 
+        /// Long is the html statuscode or -1 if some other Exception occurred.</param>
+        /// <returns>IEnumerator is some foo for the unity StartCoroutine() method</returns>
         public static IEnumerator WebRequest<T>(Uri uri, Action<T, long> callback)
         {
             UnityWebRequest www = UnityWebRequest.Get(uri.ToString());
@@ -16,28 +25,27 @@ namespace Webservice
 
             if (www.isError)
             {
+                // If the API sends a error html statuscode
                 callback(default(T), www.responseCode);
             }
             else
             {
+                // Try to deserialize the json response
                 Debug.Log("Addyi Response: " + www.downloadHandler.text);
                 try
                 {
-                    // handling if only JSON array as response is available -> [...]
-                    if (www.downloadHandler.text.StartsWith("["))
+                    string json = www.downloadHandler.text;
+
+                    // the fucking unity JSON deserializer is not able to handle arrays 
+                    // as response, so i need to hack an object around it.
+                    // To get this Object T must be ArrayResponseSQProject class
+                    if (json.StartsWith("["))
                     {
-                        string json = www.downloadHandler.text;
                         json = "{\"array\":" + json + "}";
-                        T resArrObj = JsonUtility.FromJson<T>(json);
-                        callback(resArrObj, www.responseCode);
-                    }
-                    else
-                    {
-                        T resObj = JsonUtility.FromJson<T>(www.downloadHandler.text);
-                        Debug.Log(resObj);
-                        callback(resObj, www.responseCode);
                     }
 
+                    T resObj = JsonUtility.FromJson<T>(json);
+                    callback(resObj, www.responseCode);
                 }
                 catch (Exception e)
                 {
